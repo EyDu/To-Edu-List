40ae7f2e487789d0b2d4cee198ffc4b3dfd0ba35
eydu <edu.remp33@gmail.com>
2023-04-15 17:33:35 +0200

    - (finally) added some WebMvcTests for the Endpoints.
    Milestone1 achieved!

--- Server (back-end)/to-do-list/src/test/java/com/example/todolist/controller/NoteControllerTest.java
+++ Server (back-end)/to-do-list/src/test/java/com/example/todolist/controller/NoteControllerTest.java
@@ -3,17 +3,24 @@ package com.example.todolist.controller;
 import com.example.todolist.logic.NoteService;
 import com.example.todolist.persistence.data.Note;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class NoteControllerTest {
@@ -25,6 +32,16 @@ class NoteControllerTest {
 
     @MockBean
     NoteService noteService;
+
+    private Note sampleNote1;
+
+    private Note sampleNote2;
+
+    @BeforeEach
+    public void setUp() {
+        sampleNote1 = new Note(1, "Test Note 1");
+        sampleNote2 = new Note(2, "Test Note 2");
+    }
 
     @Test
     void whenValidInput_thenReturns200() throws Exception {
@@ -43,20 +60,12 @@ class NoteControllerTest {
                         .content(objectMapper.writeValueAsString(note)))
                 .andExpect(status().isOk());
 
-        this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/notes")
-                        .contentType("application/json")
-                        .content(objectMapper.writeValueAsString(1)))
+        this.mockMvc.perform(MockMvcRequestBuilders.delete("/api/notes/1"))
                 .andExpect(status().isOk());
     }
 
     @Test
     void whenInvalidInput_thenReturns4xx() throws Exception {
-
-        //figure out how to achieve this (maybe with empty repository?)
-        /*
-        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/notes"))
-                .andExpect(status().is4xxClientError());
-         */
 
         this.mockMvc.perform(MockMvcRequestBuilders.post("/api/notes")
                         .contentType("application/json")
@@ -76,37 +85,41 @@ class NoteControllerTest {
 
     @Test
     void getNotes() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/api/notes"))
+        List<Note> notes = Arrays.asList(sampleNote1, sampleNote2);
+        given(noteService.getNotes()).willReturn(notes);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/notes"))
                 .andExpect(status().isOk())
-                .andReturn();
+                .andExpect(content().json("[{id:1,message:\"Test Note 1\"},{id:2,message:\"Test Note 2\"}]"));
 
-        String responseBody = result.getResponse().getContentAsString();
-
-        //TODO: Most likely needs to be changed later (the expected part)
-        assertEquals("[]", responseBody);
     }
 
     @Test
     void addNote() throws Exception {
-//        Note note = new Note(1, "Test note");
-//        List<Note> notes = new ArrayList<>();
-//        notes.add(note);
-//        doNothing().when(noteService).addNote(any(Note.class));
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/api/notes")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(note)))
-//                .andExpect(status().isOk());
-//
-//        assertEquals(1, notes.size());
-//        assertEquals(note, notes.get(0));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/notes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sampleNote1)))
+                .andExpect(status().isOk());
+
+        verify(noteService, times(1)).addNote(any(Note.class));
     }
 
     @Test
-    void updateNote() {
+    void updateNote() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/notes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(sampleNote1)))
+                .andExpect(status().isOk());
+
+        verify(noteService, times(1)).updateNote(any(Note.class));
     }
 
     @Test
-    void deleteNote() {
+    void deleteNote() throws Exception{
+        int id = 1;
+        mockMvc.perform(MockMvcRequestBuilders.delete("/api/notes/{id}", id))
+                .andExpect(status().isOk());
+
+        verify(noteService, times(1)).deleteNote(id);
     }
 }
